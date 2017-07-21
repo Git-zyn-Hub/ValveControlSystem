f866fe1bf4672cb004ab0523415023f4b0080c27
Git-zyn-Hub <yunanzhang0@gmail.com>
2017-07-21 09:45:56 +0800

    完成“连接“和”读取“两个菜单单击事件；完成Socket的简单收发。

---
+++ ValveControlSystem/Classes/SendDataPackage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValveControlSystem
+{
+    public class SendDataPackage
+    {
+        private const byte _frameHeader1 = 0xFF;
+        private const byte _frameHeader2 = 0x00;
+        private const byte _frameHeader3 = 0xAA;
+        private const byte _frameHeader4 = 0x55;
+        private int _checksum = 0;
+        public SendDataPackage()
+        {
+
+        }
+        /// <summary>
+        /// 数据帧头	 地址	类型	   长度    工具编号	  校验和
+        ///   4字节  1字节  1字节   1字节    1字节      2字节
+        /// </summary>
+        /// <param name="destinationAddr">目的地址</param>
+        /// <param name="dataType">类型</param>
+        /// <param name="dataContent">信息净荷</param>
+        /// <returns></returns>
+        public byte[] PackageSendData(byte destinationAddr, byte dataType, byte toolNo)
+        {
+            byte[] result;
+            //int lengthContent = 0;
+            //lengthContent = 2 + dataContent.Length;//内容长度为长度后面的字节数：信息长度+2字节校验和。
+            int lengthTotal = 10;
+            result = new byte[lengthTotal];
+            result[0] = _frameHeader1;
+            result[1] = _frameHeader2;
+            result[2] = _frameHeader3;
+            result[3] = _frameHeader4;
+            result[4] = destinationAddr;
+            result[5] = dataType;
+            result[6] = (byte)lengthTotal;
+            result[7] = toolNo;
+
+            _checksum = 0;
+            for (int i = 0; i < lengthTotal - 2; i++)
+            {
+                _checksum += result[i];
+            }
+            result[lengthTotal - 2] = (byte)((_checksum & 0xff00) >> 8);
+            result[lengthTotal - 1] = (byte)(_checksum & 0xff);
+            return result;
+        }
+    }
+}
--- ValveControlSystem/MainWindow.xaml.cs
+++ ValveControlSystem/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,124 @@ namespace ValveControlSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Socket _socketConnect;
+        private Thread _socketListenThread;
+        private SendDataPackage _sendDataPackage = new SendDataPackage();
         public MainWindow()
         {
             InitializeComponent();
+            _socketListenThread = new Thread(socketListening);
+        }
+
+        private void socketListening()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] receivedBytes = new byte[4096];
+                    int numBytes = _socketConnect.Receive(receivedBytes, SocketFlags.None);
+
+                    int checkSum;
+                    byte[] bytesActualRecv = new byte[numBytes];
+                    for (int i = 0; i < numBytes; i++)
+                    {
+                        bytesActualRecv[i] = receivedBytes[i];
+                    }
+                    //计算校验和
+                    checkSum = 0;
+                    for (int i = 0; i < bytesActualRecv.Length; i++)
+                    {
+                        if (i < bytesActualRecv.Length - 2)
+                        {
+                            checkSum += bytesActualRecv[i];
+                        }
+                    }
+                    if (bytesActualRecv.Length - 2 < 0)
+                    {
+                        MessageBox.Show("接收数据长度小于2");
+                        continue;
+                    }
+                    if (!checkCheckSum(checkSum, bytesActualRecv))
+                    {
+                        MessageBox.Show("校验和出错！");
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Socket接收线程异常" + ee.Message);
+            }
+        }
+
+        private void miReadData_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_socketConnect != null)
+                {
+                    byte[] sendData = _sendDataPackage.PackageSendData(0xff, 0x01, 0x01);
+                    _socketConnect.Send(sendData, SocketFlags.None);
+                }
+                else
+                {
+                    MessageBox.Show("Socket为空，请先连接！");
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void miConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string strIP = "192.168.1.10";
+                IPEndPoint groundBoxIP = new IPEndPoint(IPAddress.Parse(strIP), 1032);
+                _socketConnect = new Socket(groundBoxIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                _socketConnect.Connect(groundBoxIP);
+
+                MessageBox.Show("连接成功！");
+                this.miConnect.Background = new SolidColorBrush(Colors.LightGreen);
+                _socketListenThread.Start();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("连接异常：" + ee.Message);
+            }
+        }
+
+        /// <summary>
+        /// 检查校验和
+        /// </summary>
+        /// <param name="checkSum">校验和</param>
+        /// <param name="bytesReceived">接收到的全部数据，最后两字节代表校验和</param>
+        /// <returns>校验和正确返回true，错误返回false</returns>
+        private bool checkCheckSum(int checkSum, byte[] bytesReceived)
+        {
+            //判断校验和
+            int sumHigh;
+            int sumLow;
+            sumHigh = (checkSum & 0xff00) >> 8;
+            sumLow = checkSum & 0xff;
+            if (bytesReceived.Length - 2 >= 0)
+            {
+                if (sumHigh != bytesReceived[bytesReceived.Length - 2] || sumLow != bytesReceived[bytesReceived.Length - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
